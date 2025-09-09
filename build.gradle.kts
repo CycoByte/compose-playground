a419e1fff58036a3a1d8ee53431f2f7ae10ce34f
anhowes <andreas.howes@gmail.com>
2025-09-09 09:59:31 +0300

    upgrade gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.androidksp) apply false
-    kotlin("jvm") version "2.1.21" apply false
-    kotlin("plugin.serialization") version "2.1.21" apply false
+    kotlin("jvm") version "2.2.10" apply false
+    kotlin("plugin.serialization") version "2.2.10" apply false
 }
